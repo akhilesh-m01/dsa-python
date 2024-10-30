d0c1aa59239036fc92c76f31ca8d791599db7193
Akhilesh M <58871782+akhilesh-m01@users.noreply.github.com>
2024-10-30 20:22:57 +0000

    dfs

--- graphs/graph02.py
+++ graphs/graph02.py
@@ -30,11 +30,28 @@ class Graph:
                 if i not in visited:
                     q1.append(i)
                     visited.append(i)
+    
+    def dfs(self,start,vis,res):
+        vis.append(start)
+        res.append(start)
+
+        for i in self.adjList[start]:
+            if i not in vis:
+                self.dfs(i,vis,res)
+
+    def printdfs(self,start):
+        vis = []   
+        self.start = start
+        res = []
+        self.dfs(start,vis,res) 
+        print(res)   
+
 
 
 
 g1 = Graph()
-g1.addNode(1,2)
+g1.addNode(1,4)
+g1.addNode(1,3)
 g1.addNode(2,3)
 g1.addNode(3,4)
 g1.addNode(4,5)
@@ -42,5 +59,7 @@ g1.addNode(5,1)
 g1.printList()
 print("bfs implementation")
 g1.bfs(1)
+print("\ndfs implementation")
+g1.printdfs(3)
 
 
